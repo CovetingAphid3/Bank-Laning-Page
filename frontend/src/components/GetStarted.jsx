77c78508b39cd7a7c55912fe58e44584841dfe7a
Coveting Aphid <tadiwachawanda@gmail.com>
2024-02-17 17:03:14 +0200

    fix class name error

--- frontend/src/components/GetStarted.jsx
+++ frontend/src/components/GetStarted.jsx
@@ -15,7 +15,7 @@ const GetStarted = () => {
         flex-col bg-primary w-[100%] h-[100%]
         rounded-full`}
       >
-        <div class={`${styles.flexStart} flex-row`}>
+        <div className={`${styles.flexStart} flex-row`}>
           <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
             <span className="text-gradient">Get</span>
           </p>
